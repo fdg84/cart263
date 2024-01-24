1a5ceb780cc3fd7f0d549188593435a2781153d9
fdg84 <frankwell84@hotmail.com>
2024-01-23 16:17:02 -0800

    p5-speech-TEST-04 = DONE

--- Week_2/p5_speech_TEST_04/js/script.js
+++ Week_2/p5_speech_TEST_04/js/script.js
@@ -6,18 +6,22 @@ Francis Ouellette
 "use strict";
 
 const speechRecognizer = new p5.SpeechRec();
-let currentSpeech = '?';
-let lightsAreOn = false;
+// let currentSpeech = '?';
+// let lightsAreOn = false;
+let backgroundColor = 'black';
 
 function setup() {
    createCanvas(500,500);
 
     speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
     speechRecognizer.start();
 }
 
 function draw() {
-    background(0);
+    // background(0);
+    background(backgroundColor);
 
     if (lightsAreOn) {
         background(200);
@@ -25,10 +29,19 @@ function draw() {
 }
 
 function handleSpeechInput() {
-    console.log(speechRecognizer.resultString);
+    // console.log(speechRecognizer.resultString);
 
-    if (speechRecognizer.resultString.toLowerCase() === 'turn the lights on') {
-        lightsAreOn = true;
-    }
+    // if (speechRecognizer.resultString.toLowerCase() === 'turn the lights on') {
+    //     lightsAreOn = true;
+    // }
+    // else if (speechRecognizer.resultString.toLowerCase() === 'turn the lights off') {
+    //     lightsAreOn = false;
+    // }
+    // backgroundColor = speechRecognizer.resultString;
+    
+    let words = speechRecognizer.resultString.split('');
+    backgroundColor = words.pop();
+
+    
 }
     
