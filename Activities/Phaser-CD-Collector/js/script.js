5aab9616fa1dc22b439a406ac66e6895d39d789f
fdg84 <frankwell84@hotmail.com>
2024-03-11 16:06:45 -0700

    CD Collector - EDITS

--- Activities/Phaser-CD-Collector/js/script.js
+++ Activities/Phaser-CD-Collector/js/script.js
@@ -2,6 +2,8 @@
 CD Collector (Phaser Project)
 Francis Ouellette
 
+Based on PIPPIN BARR's CART 263 Class Example
+
 SOUND FX: https://pixabay.com/sound-effects/search/sparkle/ 
 AUDIO: https://github.com/phaserjs/examples/blob/master/public/src/audio/Web%20Audio/play%20movement%20sound.js
 
