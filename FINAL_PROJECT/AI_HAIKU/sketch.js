1a7edfc84c7586ff343efda6de5e88a4764d3cf5
fdg84 <frankwell84@hotmail.com>
2024-04-11 11:26:46 -0700

    edit voice

--- FINAL_PROJECT/AI_HAIKU/sketch.js
+++ FINAL_PROJECT/AI_HAIKU/sketch.js
@@ -197,6 +197,12 @@ function setup() {
   voiceRecognizer.continuous = true;
   voiceRecognizer.onResult = onResult;
   voiceRecognizer.start();
+
+  console.log(voiceSynthesizer.listVoices());
+  voiceSynthesizer.setPitch(0.1);
+  voiceSynthesizer.setRate(0.5);
+  voiceSynthesizer.setVoice(`Google UK English Female`);
+  
   textFont(`Tilt Warp`)
   startLevel()
 }
